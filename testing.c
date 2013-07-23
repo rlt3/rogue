34e548c41ddf7756f8c14f827c1e75b7aa123d14
rlt3 <leroy@bythehilt.com>
2013-07-23 19:23:26 -0400

    refactoring a lot of stuff. no more global variables

--- testing.c
+++ testing.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
@@ -7,8 +6,6 @@
 
 #include <SDL/SDL.h>
 #include <SDL_image/SDL_image.h>
-
-#include "frames.h"
 
 #define TOTAL_ENTITIES    16
 #define TILESIZE          32
@@ -50,82 +47,125 @@ typedef struct Entity {
   bool      idle;
 } Entity;
 
-/* Functions */
+SDL_Surface * load_window();
+void load_animations(SDL_Surface *sprites[], SDL_Rect frames[8][2]);
+void create_dungeon(Entity entities[], int dungeonFloor);
 
-void init();
-void game_loop();
-void render();
-void handle_input(SDLKey key);
+void game_loop(SDL_Surface *screen, 
+               SDL_Surface *sprites[], 
+               SDL_Rect frames[8][2],
+               Entity entities[],
+               int dungeonFloor);
 
-void draw_tile(uint8_t type, uint32_t x, uint32_t y);
-void draw_entity(Entity *entity);
 SDL_Surface *load_sprite(const char filename[]);
 
-void update_entity(Entity *entity, uint8_t state);
-void entity_attacks(Entity* entity);
+void render(SDL_Surface *screen, Entity *entities, 
+            SDL_Surface *sprites[], SDL_Rect frames[8][2],
+            int frameToDraw);
 
-/* Variables */
+void draw_entity(SDL_Surface *screen, Entity *entity, 
+                 SDL_Surface *sprites[], SDL_Rect frames[8][2], 
+                 int frameToDraw);
 
-SDL_Surface *screen;
-SDL_Event event;
+void draw_tile(SDL_Surface *screen, SDL_Surface *sprites[], 
+               uint8_t type, uint32_t x, uint32_t y);
 
-static SDL_Surface *sprites[TOTAL_ENTITIES];
-static Entity entities[TOTAL_ENTITIES];
+void draw(SDL_Surface *sprite, SDL_Rect *frame, 
+          SDL_Surface *screen, SDL_Rect *location);
 
-extern SDL_Rect frames[8][2];
+void handle_input(SDLKey key, Entity *Player, bool *running);
 
-unsigned short int frameToDraw;
-unsigned int t;
-bool running;
+void draw_entity(SDL_Surface *screen, Entity *entity, 
+                 SDL_Surface *sprites[], SDL_Rect frames[8][2], 
+                 int frameToDraw);
 
-int currentFloor = 0;
-
-/*
- * Using this same idea of having the delta time
- * (or current ticks) change the frame for us,
- * I can limit that frame from updating for every
- * .25 seconds or something.
- *
- * So, every .25 seconds (or .5 seconds) change
- * the global `frameToDraw' counter to the next
- */
+void update_entity(Entity entity[], uint8_t state);
+void entity_attacks(Entity* entity, Entity *entities[],
+                    Location upper, Location lower);
 
 int main(int argc, char **argv) {
-  running = true;
+  SDL_Surface *screen;
 
-  init();
-  load_frames();
-  game_loop();
+  static SDL_Surface *sprites[TOTAL_ENTITIES];
+  static Entity entities[TOTAL_ENTITIES];
+  static SDL_Rect frames[8][2];
+
+  int dungeonFloor = 0;
+
+  screen = load_window();
+  load_animations(sprites, frames);
+
+  create_dungeon(entities, dungeonFloor);
+
+  game_loop(screen, sprites, frames, entities, dungeonFloor);
 
   return EXIT_SUCCESS;
 }
 
-void init() {
+SDL_Surface * load_window() {
   SDL_Init(SDL_INIT_VIDEO);
   SDL_WM_SetCaption("Game", "Game");
   SDL_EnableKeyRepeat(70, 70);
   atexit(SDL_Quit);
+  SDL_SetVideoMode(SCREENX, SCREENY, 0, 0);
 
-  screen = SDL_SetVideoMode(SCREENX, SCREENY, 0, 0);
+  return SDL_GetVideoSurface();
+}
 
+void load_animations(SDL_Surface *sprites[], SDL_Rect frames[8][2]) {
   sprites[FLOOR]  = load_sprite("graphics/floor.png");
   sprites[ENTITY] = load_sprite("graphics/spritesheet.png"); 
 
+  frames[0][0] = (SDL_Rect){  0,  0, 64, 64};
+  frames[0][1] = (SDL_Rect){ 64,  0, 64, 64};
+  
+  frames[1][0] = (SDL_Rect){128,  0, 64, 64};
+  frames[1][1] = (SDL_Rect){192,  0, 64, 64};
+  
+  frames[2][0] = (SDL_Rect){256,  0, 64, 64};
+  frames[2][1] = (SDL_Rect){320,  0, 64, 64};
+  
+  frames[3][0] = (SDL_Rect){384,  0, 64, 64};
+  frames[3][1] = (SDL_Rect){448,  0, 64, 64};
+  
+  /* Attacking frames */
+  frames[4][0] = (SDL_Rect){  0, 64, 64, 64};
+  frames[4][1] = (SDL_Rect){ 64, 64, 64, 64};
+  
+  frames[5][0] = (SDL_Rect){128, 64, 64, 64};
+  frames[5][1] = (SDL_Rect){192, 64, 64, 64};
+  
+  frames[6][0] = (SDL_Rect){256, 64, 64, 64};
+  frames[6][1] = (SDL_Rect){320, 64, 64, 64};
+  
+  frames[7][0] = (SDL_Rect){384, 64, 64, 64};
+  frames[7][1] = (SDL_Rect){448, 64, 64, 64};
+}
+
+/* Create and load entities. Player is always the entity at [0]. */
+void create_dungeon(Entity entities[], int dungeonFloor) {
   Location location = {100, 100};
   Entity Player = (Entity){0, IDLE, 10, 0, location, location, true};
   entities[0] = Player;
 
-  int i;
-  for (i = 1; i <= currentFloor; i++) {
-    Location location = {150, 150};
-    entities[i] = (Entity){0, IDLE, 10, 0, location, location, true};
-  }
-
-  t = SDL_GetTicks();
-  frameToDraw = 0;
+  //int i;
+  //for(i = 1; i <= dungeonFloor; i++) {
+  //  Location location = {150, 150};
+  //  entities[i] = (Entity){0, IDLE, 10, 0, location, location, true};
+  //}
 }
 
-void game_loop() {
+void game_loop(SDL_Surface *screen, 
+               SDL_Surface *sprites[], 
+               SDL_Rect frames[8][2],
+               Entity entities[],
+               int dungeonFloor) {
+
+  SDL_Event event;
+
+  unsigned short int frameToDraw = 0;
+  unsigned t = SDL_GetTicks();
+  bool running = true;
 
   while (running) {
     
@@ -149,38 +189,38 @@ void game_loop() {
     while (SDL_PollEvent(&event)) {
       switch (event.type) {
         case SDL_KEYDOWN:
-          handle_input(event.key.keysym.sym);
+          handle_input(event.key.keysym.sym, &player, &running);
       }
     }
 
-    render();
+    render(screen, entities, sprites, frames, frameToDraw);
   }
 }
 
-void handle_input(SDLKey key) {
+void handle_input(SDLKey key, Entity *Player, bool *running) {
   switch(key) {
   case SDLK_ESCAPE: case SDL_QUIT:
-    running = false;
+    *running = false;
     break;
 
   case SDLK_w: case SDLK_UP: case SDLK_k:
-    update_entity(&player, WALK_UP);
+    update_entity(Player, WALK_UP);
     break;
 
   case SDLK_a: case SDLK_LEFT: case SDLK_h:
-    update_entity(&player, WALK_LEFT);
+    update_entity(Player, WALK_LEFT);
     break;
 
   case SDLK_s: case SDLK_DOWN: case SDLK_j:
-    update_entity(&player, WALK_DOWN);
+    update_entity(Player, WALK_DOWN);
     break;
 
   case SDLK_d: case SDLK_RIGHT: case SDLK_l:
-    update_entity(&player, WALK_RIGHT);
+    update_entity(Player, WALK_RIGHT);
     break;
 
   case SDLK_SPACE:
-    update_entity(&player, ATTACKING);
+    update_entity(Player, ATTACKING);
     break;
 
   default:
@@ -188,38 +228,43 @@ void handle_input(SDLKey key) {
   }
 }
 
-void render() {
+void render(SDL_Surface *screen, Entity *entities, 
+            SDL_Surface *sprites[], SDL_Rect frames[8][2],
+            int frameToDraw) {
   for (int x = 0; x < SCREENX; x++) {
     for (int y = 0; y < SCREENY; y++) {
-      draw_tile(FLOOR, x, y);
+      draw_tile(screen, sprites, FLOOR, x, y);
     }
   }
 
-  for (int i = 0; i <= currentFloor; i++) {
-    draw_entity(&entities[i]);
-  }
-
+  draw_entity(screen, &entities[0], sprites, frames, frameToDraw);
   SDL_Flip(screen);
 }
 
-void draw_tile(uint8_t type, uint32_t x, uint32_t y) {
+void draw_tile(SDL_Surface *screen, SDL_Surface *sprites[], 
+             uint8_t type, uint32_t x, uint32_t y) {
   SDL_Rect location = { x * TILESIZE, y * TILESIZE };
   SDL_Surface *sprite = sprites[type];
 
-  if(SDL_BlitSurface(sprite, NULL, screen, &location) < 0) {
-    printf("Error!\n");
-  }
+  draw(sprite, NULL, screen, &location);
 }
 
-void draw_entity(Entity *entity) {
+void draw_entity(SDL_Surface *screen, Entity *entity, 
+                 SDL_Surface *sprites[], SDL_Rect frames[8][2], 
+                 int frameToDraw) {
   int animation_frame = entity->idle? 0 : frameToDraw;
 
-  SDL_Rect spriteLocationation = { entity->location.x, entity->location.y };
+  SDL_Rect location = { entity->location.x, entity->location.y };
   SDL_Surface *sprite = sprites[ENTITY];
   SDL_Rect frame = frames[entity->state][animation_frame];
 
-  if(SDL_BlitSurface(sprite, &frame, screen, &spriteLocationation) < 0) {
-    printf("Error!\n");
+  draw(sprite, &frame, screen, &location); 
+}
+
+void draw(SDL_Surface *sprite, SDL_Rect *frame, 
+          SDL_Surface *screen, SDL_Rect *location) {
+  if(SDL_BlitSurface(sprite, frame, screen, location) < 0) {
+    printf("Error! Drawing Entity: %s\n", SDL_GetError());
   }
 }
 
@@ -247,12 +292,7 @@ SDL_Surface *load_sprite(const char filename[]) {
   return sprite;
 }
 
-void entity_attacks(Entity* entity) {
-  puts("Attacked!");
-  entity->state += 4;
-}
-
-void update_entity(Entity *entity, uint8_t state) {
+void update_entity(Entity entity[], uint8_t state) {
   entity->idle = false;
 
   if (state == ATTACKING) {
@@ -280,4 +320,8 @@ void update_entity(Entity *entity, uint8_t state) {
     break;
   }
 
+}
+
+void entity_attacks(Entity* entity, Entity *entities[],
+                    Location upper, Location lower) {
 }
